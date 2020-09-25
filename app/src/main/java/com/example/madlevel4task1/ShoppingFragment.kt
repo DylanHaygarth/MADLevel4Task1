1e952088044f681677aca979a2dcd7e3d15dc000
dylan2050 <dyhayg@gmail.com>
2020-09-25 15:58:08 +0200

    Product can be added to the shopping list

--- app/src/main/java/com/example/madlevel4task1/ShoppingFragment.kt
+++ app/src/main/java/com/example/madlevel4task1/ShoppingFragment.kt
@@ -1,6 +1,8 @@
 package com.example.madlevel4task1
 
+import android.app.AlertDialog
 import android.content.ContentValues.TAG
+import android.content.DialogInterface
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -8,16 +10,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.core.view.get
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.fragment_shopping.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class ShoppingFragment : Fragment() {
+    private lateinit var productRepository: ProductRepository
     private val products = arrayListOf<ShoppingProduct>()
     private val productAdapter = ProductAdapter(products)
+    private val mainScope = CoroutineScope(Dispatchers.Main)
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
@@ -30,7 +40,16 @@ class ShoppingFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        // Sets up the repository with the required context from this class
+        productRepository = ProductRepository(requireContext())
+        getShoppingListFromDatabase()
+
+        // sets up the recycler view
         initViews()
+
+        fabAdd.setOnClickListener {
+            showAddProductdialog();
+        }
     }
 
     private fun initViews() {
@@ -42,6 +61,71 @@ class ShoppingFragment : Fragment() {
         products.add(ShoppingProduct("kwarktaart", 6))
         products.add(ShoppingProduct("ei", 10))
         productAdapter.notifyDataSetChanged()
-        Log.d(TAG, productAdapter.getItemCount().toString())
+    }
+
+    // retrieves the products from the database. Clears all products currently in there and replaces with retrieved ones.
+    private fun getShoppingListFromDatabase() {
+        mainScope.launch {
+            val shoppingList = withContext(Dispatchers.IO) {
+                productRepository.getAllProducts()
+            }
+            this@ShoppingFragment.products.clear()
+            this@ShoppingFragment.products.addAll(shoppingList)
+            this@ShoppingFragment.productAdapter.notifyDataSetChanged()
+        }
+    }
+
+    // shows the dialog from which a product/amount can be added
+    private fun showAddProductdialog() {
+        // creates builder and sets the title of the dialog window
+        val builder = AlertDialog.Builder(requireContext())
+        builder.setTitle(getString(R.string.add_product_dialog_title))
+
+        // the layout file gets linked to the dialog
+        val dialogLayout = layoutInflater.inflate(R.layout.add_product_dialog, null)
+
+        // the filled in product/quantity
+        val productName = dialogLayout.findViewById<EditText>(R.id.etProductName)
+        val amount = dialogLayout.findViewById<EditText>(R.id.etAmount)
+
+        // the confirm button gets created
+        builder.setView(dialogLayout)
+        builder.setPositiveButton(R.string.dialog_ok_btn) { _: DialogInterface, _: Int ->
+            addProduct(productName, amount)
+        }
+        builder.show()
+    }
+
+    // adds the product to the database
+    private fun addProduct(txtProductName: EditText, txtAmount: EditText) {
+        // checks if input fields are not blank
+        if (validateFields(txtProductName, txtAmount)) {
+            mainScope.launch {
+                // creates a 'ShoppingProduct' variable from the given input fields
+                val product = ShoppingProduct(
+                    name = txtProductName.text.toString(),
+                    quantity = txtAmount.text.toString().toInt()
+                )
+
+                // inserts product into database
+                withContext(Dispatchers.IO) {
+                    productRepository.insertProduct(product)
+                }
+
+                // refreshes the shopping list
+                getShoppingListFromDatabase()
+            }
+        }
+    }
+
+    // checks if input fields are not blank
+    private fun validateFields(txtProductName: EditText, txtAmount: EditText): Boolean {
+        return if (txtProductName.text.toString().isNotBlank() && txtAmount.text.toString().isNotBlank()
+        ) {
+            true
+        } else {
+            Toast.makeText(activity, "Please fill in the fields", Toast.LENGTH_LONG).show()
+            false
+        }
     }
 }
